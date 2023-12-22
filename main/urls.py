dc30c518aeb66cff6755f4de868342a17bde7ec9
Zakhar Podporinov <zp162162@gmail.com>
2023-12-22 19:43:05 +0200

    Include account and posts_app urls

--- main/urls.py
+++ main/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from auth_app.views import register_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("posts_app.urls", namespace="Post")),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/register/", register_view, name="register")
 ]
