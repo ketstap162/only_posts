02b2cfb401e196cd03b5d885280f3d78ddd21bc9
Zakhar Podporinov <zp162162@gmail.com>
2023-12-22 19:44:39 +0200

    Create register_view in auth_app

--- auth_app/views.py
+++ auth_app/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def register_view(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+
+    context = {"form": form}
+    return render(request, "registration/register.html", context)
+
+
+# def profile_view(request):
+#     if not request.user.is_authenticated:
+#         return redirect("login")
+#
+#     return redirect("VPN:home")
