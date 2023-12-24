b61335542a6f63d2ea1f629ed92db1ec09337456
Zakhar Podporinov <zp162162@gmail.com>
2023-12-24 23:02:20 +0200

    Add attachment field and clean_text method in Post model in posts_app/models.py

--- posts_app/models.py
+++ posts_app/models.py
@@ -1,9 +1,10 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils import timezone
-
+from bleach import clean
 
 # Create your models here.
+from posts_app.utils.models import attachment_file_path
 
 
 class TimeStampedModel(models.Model):
@@ -24,6 +25,18 @@ class Post(TimeStampedModel):
         null=True,
         default=None
     )
+    attachment = models.FileField(null=True, upload_to=attachment_file_path)
+
+    def clean_text(self):
+        allowed_tags = ["a", "code", "i", "strong"]
+
+        cleaned_text = clean(
+            str(self.text),
+            tags=allowed_tags,
+            attributes={"a": ["href", "title"]}
+        )
+
+        return cleaned_text
 
     class Meta:
         ordering = ["-created_at"]
