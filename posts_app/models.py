70204043097e6dfbcc6e34a0f8511f070a32ebc9
Zakhar Podporinov <zp162162@gmail.com>
2023-12-25 05:25:45 +0200

    Update attachment, add attachment_type property and define objects manager in Post model in posts_app/models.py

--- posts_app/models.py
+++ posts_app/models.py
@@ -1,10 +1,12 @@
+import os
+
+from bleach import clean
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils import timezone
-from bleach import clean
 
-# Create your models here.
-from posts_app.utils.models import attachment_file_path
+from posts_app.posts_settings import ALLOWED_IMAGE_EXTENSIONS
+from posts_app.utils.model_utils import attachment_file_path, check_attachment_extensions
 
 
 class TimeStampedModel(models.Model):
@@ -25,7 +27,23 @@ class Post(TimeStampedModel):
         null=True,
         default=None
     )
-    attachment = models.FileField(null=True, upload_to=attachment_file_path)
+    attachment = models.FileField(
+        null=True,
+        default=None,
+        upload_to=attachment_file_path
+    )
+
+    objects = models.Manager()
+
+    @property
+    def attachment_type(self):
+        _, extension = os.path.splitext(self.attachment.path)
+        if extension in ALLOWED_IMAGE_EXTENSIONS:
+            print("image")
+            return "image"
+        elif extension in ALLOWED_IMAGE_EXTENSIONS:
+            print("txt")
+            return "txt"
 
     def clean_text(self):
         allowed_tags = ["a", "code", "i", "strong"]
@@ -33,10 +51,15 @@ class Post(TimeStampedModel):
         cleaned_text = clean(
             str(self.text),
             tags=allowed_tags,
-            attributes={"a": ["href", "title"]}
         )
 
         return cleaned_text
+
+    def save(self, *args, **kwargs):
+        if self.attachment:
+            check_attachment_extensions(str(self.attachment))
+
+        super().save(*args, **kwargs)
 
     class Meta:
         ordering = ["-created_at"]
