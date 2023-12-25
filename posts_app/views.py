d66240d72e0d6a037272e9beef1a0e9361a53c3c
Zakhar Podporinov <zp162162@gmail.com>
2023-12-25 05:23:51 +0200

    Update index() view and add create_post() view

--- posts_app/views.py
+++ posts_app/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from posts_app.forms import PostForm
+from posts_app.models import Post
 
 
 def index(request):
-    return render(request, "posts/posts.html")
+    context = {
+        "posts": Post.objects.all()
+    }
+    return render(request, "posts/posts.html", context=context)
+
+
+def create_post(request):
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.owner = request.user
+            post.save()
+            return redirect("Post:home")
+    else:
+        form = PostForm()
+
+    return render(request, "posts/post-create.html", {"form": form})
