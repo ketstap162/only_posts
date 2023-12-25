8dc2d13a12411f2459acf46601a805b8bc4d1e3c
Zakhar Podporinov <zp162162@gmail.com>
2023-12-25 05:26:41 +0200

    Fix process_uploaded_image() in posts_app/signals.py

--- posts_app/signals.py
+++ posts_app/signals.py
@@ -1,10 +1,15 @@
+import os
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from posts_app.models import Post
-from posts_app.utils.models import process_image
+from posts_app.posts_settings import ALLOWED_IMAGE_EXTENSIONS
+from posts_app.utils.model_utils import process_image
 
 
 @receiver(post_save, sender=Post)
 def process_uploaded_image(sender, instance, **kwargs):
-    if instance.image:
-        process_image(instance.image.path)
+    if instance.attachment:
+        _, extension = os.path.splitext(instance.attachment.path)
+        if extension in ALLOWED_IMAGE_EXTENSIONS:
+            process_image(instance.attachment.path)
