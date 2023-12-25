c8ceac9c24f4c80a1dea390986b2f2f04a1bb069
Zakhar Podporinov <zp162162@gmail.com>
2023-12-25 05:24:09 +0200

    Update urlpatterns in posts_app/urls.py

--- posts_app/urls.py
+++ posts_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from posts_app.views import index
+from posts_app.views import index, create_post
 
 app_name = "Post"
 
 urlpatterns = [
-    path("", index, name="home")
+    path("", index, name="home"),
+    path("create/", create_post, name="create"),
 ]
