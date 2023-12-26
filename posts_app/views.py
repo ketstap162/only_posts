fcee1eb42c9721d1a69131fdc6a80c0c77cf4bab
Zakhar Podporinov <zp162162@gmail.com>
2023-12-26 18:34:39 +0200

    Add pagination, ordering and filtering in index view in posts_app/views.py

--- posts_app/views.py
+++ posts_app/views.py
@@ -1,4 +1,4 @@
-
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, redirect
 
 from http_utils.account_tools import login_required
@@ -9,8 +9,30 @@ from django.core.exceptions import ObjectDoesNotExist
 
 
 def index(request):
+    posts = Post.objects.filter(replied=None)
+    sort_order = request.GET.get("sort_by_date", 0)
+    username_filter = request.GET.get("username_filter")
+
+    if sort_order:
+        posts = posts.order_by(f"{'' if sort_order == 'old' else '-'}created_at")
+
+    if username_filter:
+        posts = posts.filter(owner__username__startswith=username_filter)
+
+    posts_per_page = int(request.GET.get("posts_per_page", 25))
+
+    paginator = Paginator(posts, posts_per_page)
+    page = request.GET.get("page")
+
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
     context = {
-        "posts": Post.objects.filter(replied=None)
+        "posts": posts
     }
     return render(request, "posts/posts.html", context=context)
 
