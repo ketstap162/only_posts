a81c3321f28cc8b63778375229f9a8bf34c9190e
Zakhar Podporinov <zp162162@gmail.com>
2023-12-26 16:24:16 +0200

    Add IMAGE_MAX_WIDTH and IMAGE_MAX_HEIGHT in posts_app/posts_settings.py

--- posts_app/posts_settings.py
+++ posts_app/posts_settings.py
@@ -1,3 +1,5 @@
 ALLOWED_TEXT_FILE_EXTENSIONS = (".txt",)
 ALLOWED_IMAGE_EXTENSIONS = (".jpg", ".jpeg", ".gif", ".png")
 ALLOWED_POST_TAGS = ("a", "code", "i", "strong")
+IMAGE_MAX_WIDTH = 320
+IMAGE_MAX_HEIGHT = 240
